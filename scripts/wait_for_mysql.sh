8d0909b3815cab967951a433f1797c0fb88b47ac
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-11-22 06:48:28 +0100

    maj

---
+++ scripts/wait_for_mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+########## Check script to ensure MySQL is ready before starting replication on slave host ##########
+
+# host name (container mysql_master) is passed as an argument
+host=$1
+shift
+
+# Loop to check if mysql is ready to receive connections
+until mysql -h "$host" -u root -p"$MYSQL_ROOT_PASSWORD" -e 'show databases'; do
+  echo "Waiting for MySQL at $host to be ready..."
+  sleep 2
+done
+
+# Running command after mysql is ready on master server
+exec "$@"
