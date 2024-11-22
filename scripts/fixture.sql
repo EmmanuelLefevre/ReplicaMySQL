94340649ffd990eab8794fa70e5dbf430280fd2a
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2024-11-22 06:48:22 +0100

    maj

---
+++ scripts/fixture.sql
@@ -0,0 +1,16 @@
+SET NAMES 'utf8';
+SET CHARACTER SET utf8;
+SET SESSION collation_connection = 'utf8_general_ci';
+
+CREATE TABLE IF NOT EXISTS test (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO test (nom, prenom)
+VALUES
+('Lefevre', 'Emmanuel'),
+('Adulyadej', 'Bhumibol'),
+('Poutine', 'Vladimir'),
+('Zelensky', 'Volodymyr');
