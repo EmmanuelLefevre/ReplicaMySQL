4fc139e13b07be16a0eb0c386a60228223ed00b1
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-01-21 18:52:27 +0100

    maj

--- scripts/set_permissions_master_cnf.sh
+++ scripts/set_permissions_master_cnf.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Vérifier que le fichier de configuration existe
 if [ -f /etc/mysql/conf.d/master.cnf ]; then
   echo "Fixing permissions for master.cnf..."
-  chmod 644 /etc/mysql/conf.d/master.cnf  # Donne des permissions de lecture et écriture pour le propriétaire et de lecture pour les autres
-  chown mysql:mysql /etc/mysql/conf.d/master.cnf  # Changer le propriétaire à mysql
+  chmod 644 /etc/mysql/conf.d/master.cnf
+  chown mysql:mysql /etc/mysql/conf.d/master.cnf
   echo "Permissions for master.cnf updated successfully."
 else
   echo "master.cnf not found, skipping permission fix."
--- scripts/set_permissions_slave_cnf.sh
+++ scripts/set_permissions_slave_cnf.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Vérifier que le fichier de configuration existe
 if [ -f /etc/mysql/conf.d/slave.cnf ]; then
   echo "Fixing permissions for slave.cnf..."
-  chmod 644 /etc/mysql/conf.d/slave.cnf  # Donne des permissions de lecture et écriture pour le propriétaire et de lecture pour les autres
-  chown mysql:mysql /etc/mysql/conf.d/slave.cnf  # Changer le propriétaire à mysql
+  chmod 644 /etc/mysql/conf.d/slave.cnf
+  chown mysql:mysql /etc/mysql/conf.d/slave.cnf
   echo "Permissions for slave.cnf updated successfully."
 else
   echo "slave.cnf not found, skipping permission fix."
